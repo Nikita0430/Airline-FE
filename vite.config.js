8f2a65d4a7beb8ad72f43394ae5c8dd250b08ade
Nikita <23354.nikita@gmail.com>
2025-02-19 12:28:53 +0530

    updated base in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:"https://github.com/Nikita0430/Airline-FE"
+  base:"/Airline-FE"
 })
